a814b0a0868e9d5760fb90f73868ec55a10b7f98
Emrah Nazif <emrah@ayofi.com>
2026-09-23 19:13:37 +0200

    Remove the unused dataset catalog

--- src/mayaku/data/__init__.py
+++ src/mayaku/data/__init__.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from mayaku.data.augment import CLEAN_AUG, DEFAULT_AUG, Augment
 from mayaku.data.batch import batch_to, collate
-from mayaku.data.catalog import DatasetCatalog, Metadata, default_catalog
 from mayaku.data.dataset import CocoDetection
 from mayaku.data.serialize import SerializedList
 from mayaku.data.shared import load_shared_dataset
@@ -14,11 +13,8 @@ __all__ = [
     "DEFAULT_AUG",
     "Augment",
     "CocoDetection",
-    "DatasetCatalog",
-    "Metadata",
     "SerializedList",
     "batch_to",
     "collate",
-    "default_catalog",
     "load_shared_dataset",
 ]
